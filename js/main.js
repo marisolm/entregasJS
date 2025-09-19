c311a32cece417d518991f6c1f833a62a5322728
MARECOS <msmareco@ab-inbev.com>
2025-09-19 12:58:08 -0300

    se agrega funcion de sumas totales

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
-
-
-/* Ejercicio 1 */
-
-// let number1 = 0;
-// let string1 = "String";
-// let boolean1 = false; 
-
-// console.log("number:", number1);
-// console.log("string:", string1);
-// console.log("boolean:", boolean1);
-
-
-
-
-/* Ejercicio 2 */
-
 // cantidad de números que desea sumar - parseado a int
+function calculadora() {
+  let continuar = true;
+  let resultados = [];
+  let verSumaTotal = true;
 
-let continuar = true;
+  while(continuar){
+    let cantidad = parseInt(prompt("Ingresa la cantidad de números que desea sumar:"));
+    console.log("los números a sumar son:", cantidad);
 
-while(continuar){
-  let cantidad = parseInt(prompt("Ingresa la cantidad de números que desea sumar:"));
-  console.log("los números a sumar son:", cantidad);
+    let suma = 0;
 
-  let suma = 0;
+    for (let i = 0; i < cantidad; i++) {
+      let numero = parseInt(prompt("Ingresa el número " +(i+1) +":"));
+      suma += numero;
 
-  for (let i = 0; i < cantidad; i++) {
-    let numero = parseFloat(prompt(`Ingresa el número ${i+1}:`));
-    suma += numero;
-  };
+    };
 
-  alert("La suma final es: " + suma);
-  continuar = confirm ("Desea hacer otra suma?");
-}
+    alert("La suma final es: " + suma);
+    resultados.push(suma);
+    continuar = confirm ("Desea hacer otra suma?");
+  }
+    //mostrar la suma de las operaciones realizadas 
+    verSumaTotal = confirm ("Desea sumar los resultados de todas las operaciones realizadas?");
+    if (verSumaTotal){
+      suma = 0; 
+      for (let i = 0; i < resultados.length; i++){
+        suma = suma + resultados[i];
+      }
+      alert ("la suma total es " + suma);
+    }
+};
+
+//llamada a la funcion para inciar las operaciones matemáticas
+calculadora(); 
