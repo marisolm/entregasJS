5597513d0ff99fda66dcf598a7344d0b96a06ef9
MARECOS <msmareco@ab-inbev.com>
2025-09-19 12:59:49 -0300

    limpieza de codigo y adicion de comentarios en el codigo

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function calculadora() {
   let resultados = [];
   let verSumaTotal = true;
 
+  //continua mientras el usuario desee hacer más operaciones
   while(continuar){
     let cantidad = parseInt(prompt("Ingresa la cantidad de números que desea sumar:"));
     console.log("los números a sumar son:", cantidad);
@@ -17,18 +18,20 @@ function calculadora() {
     };
 
     alert("La suma final es: " + suma);
-    resultados.push(suma);
+    resultados.push(suma); // acumula los resultados para la suma final 
     continuar = confirm ("Desea hacer otra suma?");
   }
-    //mostrar la suma de las operaciones realizadas 
-    verSumaTotal = confirm ("Desea sumar los resultados de todas las operaciones realizadas?");
-    if (verSumaTotal){
-      suma = 0; 
-      for (let i = 0; i < resultados.length; i++){
-        suma = suma + resultados[i];
-      }
-      alert ("la suma total es " + suma);
+
+  //mostrar la suma de las operaciones realizadas 
+  verSumaTotal = confirm ("Desea sumar los resultados de todas las operaciones realizadas?");
+  
+  if (verSumaTotal){
+    suma = 0; 
+    for (let i = 0; i < resultados.length; i++){
+      suma = suma + resultados[i];
     }
+    alert ("la suma total es " + suma);
+  }
 };
 
 //llamada a la funcion para inciar las operaciones matemáticas
