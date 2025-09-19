3db92e7eadb8e0ebeec80e40470ebf3c678b53e0
MARECOS <msmareco@ab-inbev.com>
2025-09-19 12:31:51 -0300

    se agrega segunda funcion (while)

--- js/main.js
+++ js/main.js
@@ -16,14 +16,20 @@
 /* Ejercicio 2 */
 
 // cantidad de números que desea sumar - parseado a int
-let cantidad = parseInt(prompt("Ingresa la cantidad de números que desea sumar:"));
-console.log("los números a sumar son:", cantidad);
 
-let suma = 0;
+let continuar = true;
 
-for (let i = 0; i < cantidad; i++) {
-  let numero = parseFloat(prompt(`Ingresa el número ${i+1}:`));
-  suma += numero;
-};
+while(continuar){
+  let cantidad = parseInt(prompt("Ingresa la cantidad de números que desea sumar:"));
+  console.log("los números a sumar son:", cantidad);
 
-alert("La suma final es: " + suma);
+  let suma = 0;
+
+  for (let i = 0; i < cantidad; i++) {
+    let numero = parseFloat(prompt(`Ingresa el número ${i+1}:`));
+    suma += numero;
+  };
+
+  alert("La suma final es: " + suma);
+  continuar = confirm ("Desea hacer otra suma?");
+}
